a0546fc6e222e68b529f84bf34da38c7f02e9d1d
Bilal <mrbilal.programmer@gmail.com>
2018-09-12 21:02:39 +0500

    Add files via upload

---
+++ mark.el
@@ -0,0 +1,25 @@
+(defun hov ()
+  (interactive)
+  
+  (message "h")
+  (buffer-substring-no-properties (mark) (point)))
+
+(defun evali (in)
+  
+  (message in)
+  (cadr (slime-eval `(swank:eval-and-grab-output ,in))))
+
+
+
+(defun selct ()
+  (interactive)
+  (backward-up-list)
+  (mark-sexp)
+  (evali (concat "(echo:send (cl-igloo:ps " (hov) ") \"" (substring (buffer-name) 0 -6) "\")")))
+
+
+
+(define-key lisp-mode-map [remap isearch-forward] 'selct)
+
+
+;(substring "script0" 0 -1)
